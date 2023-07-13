46edb7c0cbfe6d73a5697cfffaef5f7baff42267
Ana Beatriz Gonçalves <ana.goncalves@itsector.pt>
2023-07-13 11:43:21 +0100

    Removed logout action on initial screen and moved to NavbarComponent to avoid repeating code on all screens using NavbarComponent.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,20 +1,34 @@
 import { Button, Container, Navbar } from "react-bootstrap";
+import { useStore } from "../../store/store";
+import { useCallback } from "react";
+import { UserAuthentication } from "../../screens/loginSignup/models/user";
 
 interface NavbarProps {
-  userLogged: string | undefined;
-  logout: () => void;
+  dispatch: (action: any) => void;
 }
 
-const NavbarComponent: React.FC<NavbarProps> = ({ userLogged, logout }) => {
+const NavbarComponent: React.FC<NavbarProps> = ({ dispatch }) => {
+  const userWithAutentication = useStore(
+    (state) => state.usersWithAuthentication
+  );
+  const userLogout = userWithAutentication.find((user) => user.isLogged);
+
+  const logout = useStore((state) => state.logout);
+
+  const onHandleLogoutAction = useCallback(() => {
+    logout(userLogout as UserAuthentication);
+    dispatch("logout");
+  }, [userWithAutentication]);
+
   return (
     <div>
       <Navbar className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">Welcome, {userLogged}</Navbar.Brand>
+          <Navbar.Brand href="#home">Welcome, {userLogout?.name}</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-              <Button variant="info" onClick={logout}>
+              <Button variant="info" onClick={onHandleLogoutAction}>
                 Logout
               </Button>
             </Navbar.Text>
